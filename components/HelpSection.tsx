afa72af46c4a2bc9250c61b258514758614c22a1
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-04-05 22:38:34 +0100

    help responsiveness

--- components/HelpSection.tsx
+++ components/HelpSection.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import Image from "next/image"
 import bgImage from "../Assets/images/Image.png"
+import help from "../Assets/images/help.png"
+
 const HelpSection = () => {
+  
   return (
-    <section className='relative h-screen'>
+    <section className='lg:px-[210px] md:px-[180px] px-4 h-screen relative '>
         <div className='absolute z-10 top-0 backdrop-blur-lg bottom-0 left-0 right-0 bg-[#2E4049E8]'></div>
         <Image 
         src={bgImage}
-        alt={''}
+        alt={'blur bg'}
         fill
         className=' '
         />
-        <div className='z-20'>
-            <div className='z-20'>
+        <div className='grid lg:grid-cols-2 gap-9 items-center h-screen z-10 my-auto justify-center'>
+            <div className='z-20 space-y-4'>
                 <p className='hero_text_sm z-20 text-white'>How We Help</p>
+                <p className='hero_text_big text-[20px] lg:text-[35px] xl:text-[35px] 2xl:text-[40px] font-extrabold text-white'>Join The CommunityTo Give Education For Children</p>
+                <p className='text-white text-[15px] 2xl:text-[35px]'>Lorem Ipsum is simply dummy text of the printin typesetting  dummy text ever  when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                <Image 
+                src={help}
+                alt='helpImg'
+                width={0}
+                height={0}
+                className='object-contain'
+                />
+                <button className='bg-[#FF6D6D] text-white big_btn' >DONATE NOW</button>
             </div>
-            <div></div>
+            <div className='border hidden lg:block py-7 z-20 border-white px-10'>
+              <p className='hero_text_big pb-5 text-[20px] lg:text-[35px] xl:text-[35px] 2xl:text-[40px] font-extrabold text-white'>Become A Volunteer</p>
+              <form className='flex flex-col gap-5'>
+                  <input className='border w-full bg-transparent border-white p-3' type='text' placeholder="Full Name*"/>
+                  <input className='border w-full bg-transparent border-white p-3' type='email' placeholder="Email Address*"/>
+                  <input className='border w-full bg-transparent border-white p-3' type='phone' placeholder="Phone Number*"/>
+                  <textarea />
+              </form>
+            </div>
         </div>
     </section>
   )
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -15,16 +15,16 @@ const HeroSection = () => {
             <h1 className='hero_text text-white font-normal text-[30px] md:text-[50px]'>Need Help...</h1>
             <p className="hero_text_big text-white text-[40px] md:text-[50px] lg:text-[70px] font-extrabold">Being Life Saver</p>
             <p className="hero_text_big text-white text-[40px] md:text-[50px] lg:text-[70px] font-extrabold">For Someone</p>
-        </div>
-        <div className=' z-10 items-center hidden lg:flex md:max-w-[520px]'>
-            <div className='w-3 h-3 bg-white rounded-full'></div>
-            <div className='h-[1px] md:w-[310px] bg-white'></div>
-            <div className="border px-2 py-2 flex-1 rounded-full text-white">BELOW POVERTY LINE</div>
-        </div>
-      <div className='z-10 flex-start 2xl:mt-5 items-start flex gap-4 '>
+          </div>
+          <div className=' z-10 items-center hidden lg:flex md:max-w-[520px]'>
+              <div className='w-3 h-3 bg-white rounded-full'></div>
+              <div className='h-[1px] md:w-[310px] bg-white'></div>
+              <div className="border px-2 py-2 flex-1 rounded-full text-white">BELOW POVERTY LINE</div>
+          </div>
+          <div className='z-10 flex-start 2xl:mt-5 items-start flex gap-4 '>
             <button className='py-3 hero_text_big px-6 text-white font-normal bg-[#FF6D6D] rounded-full'>Donate</button>
             <button className='py-3 hero_text_big px-6  font-normal bg-white rounded-full'>Discover</button>
-        </div>
+          </div>  
       </div>
         
         
